5b90ffe98e59ac80466385936f666f004375063e
Beatriz Bernardo <anabeatrizbernardo@hotmail.com>
2016-11-15 15:52:40 +0000

    node_modules

--- src/js/client.js
+++ src/js/client.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 class Layout extends React.Component {
     render(){
         return(
-            <h1> primeiro teste </h1>
+            <h1> primeiro teste</h1>
         );
     }
 }
