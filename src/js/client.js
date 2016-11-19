8e38cfaf6012313185494e03323cebffae5db4b7
Beatriz Bernardo <anabeatrizbernardo@hotmail.com>
2016-11-19 20:29:32 +0000

    falta a interação entre páginas diferentes

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory, DefaultRoute, Link, RouteHandler } from "react-router";
 
 
 import PaginaPrincipal from "./pages/PaginaPrincipal";
+import PaginaEvento from "./pages/PaginaEvento";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<PaginaPrincipal/>,app);
+/**
+ * render principal
+ */
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={PaginaPrincipal}>
+            <Route path="paginaEvento" component={PaginaEvento}></Route>
+        </Route>
+    </Router>,
+    app);
+
+
+/**
+ * só para testes da paginaEvento
+ */
+/*ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={PaginaEvento}>
+            
+        </Route>
+    </Router>,
+    app);*/
--- src/js/components/MostrarEventos.js
+++ src/js/components/MostrarEventos.js
@@ -1,4 +1,11 @@
 import React from "react";
+import Router from 'react-router';  
+import { DefaultRoute, Link, Route, RouteHandler, IndexLink } from 'react-router';
+
+import PaginaEvento from "../pages/PaginaEvento";
+import Evento from "../components/Evento";
+import * as EventoAction from "../actions/EventoAction";
+import EventoStore from "../stores/EventoStore";
 
 {
     /* Este Component so serve para mostrar os próximos eventos 
@@ -6,18 +13,54 @@ import React from "react";
 }
 
 export default class MostrarEventos extends React.Component {
-
-  render() {
+    constructor(){
+        super();
+        this.getEventos = this.getEventos.bind(this);
+        this.state = {
+            eventos: EventoStore.getAll(),
+        };
+    }
     
-    return (
-        <div class="list-group">
-            <a href="#" class="list-group-item active">
-                Next Events
-            </a>
-            <a href="#" class="list-group-item">Ver o Pai Natal</a>
-            <a href="#" class="list-group-item">Passagem de Ano</a>
-            <a href="#" class="list-group-item">Fazer árvore de Natal</a>
-        </div>
-    );
-  }
+    componentWillMount(){
+        EventoStore.on("change", this.getEventos);
+    }
+
+    componentWillUnmount(){
+        EventoStore.removeListener("change", this.getEventos);
+    }
+
+    getEventos(){
+        this.setState({
+            eventos: EventoStore.getAll(),
+        });
+    }
+
+//    reloadEventos(){
+//        EventoAction.reloadEventos();
+//    }
+
+/* adoptar o mesmo sistema para boleias dadas e recebidas*/
+    render() {
+        
+        const { eventos } = this.state;
+        const EventoComponents = eventos.map((evento) => {
+            return <Link to={'paginaEvento'} class="list-group-item" key={evento.id}>
+                        <span>   {evento.nomeEvento}   </span>
+                        <span>   <i class="fa fa-chevron-right" aria-hidden="true"></i>   </span> 
+                        <span>   {evento.dataInicio}   </span>
+                        <span>   <i class="fa fa-chevron-right" aria-hidden="true"></i>   </span> 
+                        <span>   {evento.local}   </span>
+                    </Link>
+        });
+
+        return (
+            <div class={"list-group"}>
+                <a class="list-group-item active">
+                    Next Events
+                </a>
+                { EventoComponents }
+                {this.props.children}
+            </div>
+        );
+    }
 }
